77b3d07af30cb1d08a82b3157f0af62ebe067027
Riyadhifalsf <gbzleopard@gmail.com>
2023-06-06 23:29:00 +0700

    mengganti codingan dengan codingan yang baru

--- script.js
+++ script.js
@@ -20,34 +20,57 @@ function renderBoard() {
   puzzleBoard.innerHTML = "";
 
   for (var i = 0; i < images.length; i++) {
-    var tile = document.createElement("div");
-    if (images[i] !== "") {
-      var img = document.createElement("img");
-      img.src = images[i];
-      tile.appendChild(img);
-    }
-    tile.setAttribute("onclick", "moveTile(" + i + ")");
-    puzzleBoard.appendChild(tile);
+      var tile = document.createElement("div");
+      tile.className = "tile";
+      tile.dataset.index = i;
+      tile.style.backgroundImage = "url(" + images[i] + ")";
+      tile.addEventListener("click", clickTile);
+      puzzleBoard.appendChild(tile);
+
+      if (images[i] === "") {
+          tile.classList.add("empty");
+          tile.classList.remove("tile");
+          tile.removeEventListener("click", clickTile);
+      }
   }
 }
 
-function moveTile(index) {
-  if (isValidMove(index)) {
-    var emptyIndex = images.indexOf("");
-    swapTiles(index, emptyIndex);
-    renderBoard();
+function clickTile(e) {
+  var tileIndex = parseInt(e.target.dataset.index);
+  var emptyTileIndex = getEmptyTileIndex();
 
-    if (isSolved()) {
-      alert("Selamat! Anda menyelesaikan puzzle.");
-    }
+  if (isAdjacent(tileIndex, emptyTileIndex)) {
+      swapTiles(tileIndex, emptyTileIndex);
+      renderBoard();
+
+      if (isSolved()) {
+          alert("Selamat! Anda menyelesaikan puzzle.");
+      }
   }
 }
 
-function isValidMove(index) {
-  var emptyIndex = images.indexOf("");
-  var rowDiff = Math.abs(Math.floor(emptyIndex / 3) - Math.floor(index / 3));
-  var colDiff = Math.abs((emptyIndex % 3) - (index % 3));
-  return (rowDiff + colDiff === 1);
+function getEmptyTileIndex() {
+  var tiles = document.querySelectorAll(".tile");
+  for (var i = 0; i < tiles.length; i++) {
+      if (tiles[i].classList.contains("empty")) {
+          return i;
+      }
+  }
+  return -1;
+}
+
+function isAdjacent(tileIndex, emptyTileIndex) {
+  var rowSize = Math.sqrt(images.length);
+  var row = Math.floor(tileIndex / rowSize);
+  var col = tileIndex % rowSize;
+
+  var emptyRow = Math.floor(emptyTileIndex / rowSize);
+  var emptyCol = emptyTileIndex % rowSize;
+
+  return (
+      (Math.abs(row - emptyRow) === 1 && col === emptyCol) ||
+      (Math.abs(col - emptyCol) === 1 && row === emptyRow)
+  );
 }
 
 function swapTiles(index1, index2) {
@@ -58,9 +81,9 @@ function swapTiles(index1, index2) {
 
 function isSolved() {
   for (var i = 0; i < images.length - 1; i++) {
-    if (images[i] !== "image" + (i + 1) + ".jpg") {
-      return false;
-    }
+      if (images[i] !== "image" + (i + 1) + ".jpg") {
+          return false;
+      }
   }
   return true;
 }
@@ -69,11 +92,11 @@ function shuffle(array) {
   var currentIndex = array.length, temporaryValue, randomIndex;
 
   while (currentIndex !== 0) {
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
   }
 
   return array;
